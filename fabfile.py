35ece2c42640058736620d53da183c1440e6f810
Сергей Самойлов <samojlov_sg@astralnalog.ru>
2016-08-08 14:51:35 +0300

    Были добавлены основные методы для работы с проектому удаленно

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,38 @@
 # coding: utf-8
 import os
+
 from fabric.api import run, env, cd, roles
 from settings.default_settings import *
 from settings.secret_settings import *
 
 # Списком можно перечислить несколько серверов, которые у вас считаются "продакшеном"
-env.roledefs[SERVER] = [USER + '@' + HOST + ':' + PORT]
+env.roledefs[SERVER] = [USER + '@' + HOST + ':' + str(PORT)]
+
+
+def change_server_state(action='restart'):
+    run('echo {password} | sudo -S service {server_name} {action}'.format(server_name=WORK_SERVER,
+                                                                          password=PASSWORD,
+                                                                          action=action))
+    # sudo('service {server_name} {action}'.format(server_name=WORK_SERVER, action=action),
+    #      user=USER, shell=False)
+
+
+def do(action):
+    production_env()
+    with cd(env.project_root):
+        run(action)
+
+
+def pip(action):
+    production_env()
+    with cd(env.project_root):
+        run('{pip} {action}'.format(pip=env.pip, action=action))
+
+
+def python(action):
+    production_env()
+    with cd(env.project_root):
+        run('{python} {action}'.format(python=env.python, action=action))
 
 
 def production_env():
@@ -13,37 +40,68 @@ def production_env():
     env.key_filename = PATH_KEY
     env.user = USER  # На сервере будем работать из под пользователя
     env.password = PASSWORD
-    root = PATH
-    run('mkdir -p {}'.format(root))
-    env.project_root = root  # Путь до каталога проекта (на сервере)
-    env.python = os.path.join(PATH, '/venv/bin/python')
-    env.pip = os.path.join(PATH, 'myprojects/python/fabtest/venv/bin/pip')
+    env.project_root = PATH  # Путь до каталога проекта (на сервере)
+    env.python = PYTHON
+    env.pip = PIP
 
 
-@roles('production')
-def init_production():
+def venv_update():
+    production_env()
+    with cd(env.project_root):
+        run('rm -rf venv')
+        run('virtualenv venv')
+        run('{pip} install -r {requirements}'.format(pip=env.pip, requirements=VENV_REQUIREMENTS))
+
+
+@roles(SERVER)
+def init():
+    run('mkdir -p {}'.format(PATH))
     production_env()
     with cd(env.project_root):
         run('git init')
-        run('git remote rm origin')
-        run('git remote add origin {git_url}'.format(REPOSITORY))
+        # run('git remote set-url origin {repository}'.format(repository=REPOSITORY+'.git'))
+        run('git remote add origin {repository}'.format(repository=REPOSITORY))
         run('echo {password} | sudo -S apt-get -y install virtualenv'.format(password=PASSWORD))
         run('virtualenv venv')
 
 
-@roles('production')
-def deploy_production():
+@roles(SERVER)
+def change():
+    production_env()
+    with cd(env.project_root):
+        run('git remote set-url origin {repository}'.format(repository=REPOSITORY))
+
+
+@roles(SERVER)
+def deploy():
     production_env()  # Инициализация окружения
     with cd(env.project_root):  # Заходим в директорию с проектом на сервере
         run('git pull {branch} {fetch}'.format(branch='origin', fetch='master'))  # Пуляемся из репозитория
+        control_service(action='restart')
 
 
-@roles('production')
-def pip_install():
+@roles(SERVER)
+def rollback():
     production_env()
-    run('{pip} install --upgrade -r {filepath}'.format(pip=env.pip,
-                                                       filepath=os.path.join(env.project_root, 'requirements.txt')))
+    with cd(env.project_root):
+        run('git reset --hard HEAD^')
+        control_service(action='restart')
 
+
+
+# @roles(SERVER)
+# def create_dir():
+# do('mkdir something')
+# python('hello.py')
+# pip('install SQLAlchemy')
+
+
+@roles(SERVER)
+def control_service(service=WORK_SERVER, action='restart'):
+    production_env()
+    run('echo {password} | sudo -S service {server_name} {action}'.format(server_name=service,
+                                                                          password=PASSWORD,
+                                                                          action=action))
 
 
 
--- settings/default_settings.py
+++ settings/default_settings.py
@@ -1,6 +1,23 @@
+# -*- coding: utf-8 -*-
 import os
+from settings.secret_settings import *
 
 SERVER = "production"
 PORT = 22
-BRANCH = "master"
+BRANCH = "dev"
 PATH_KEY = [os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')]  # Локальный путь до файла с ключами
+PYTHON = os.path.join(PATH, 'venv/bin/python')
+PIP = os.path.join(PATH, 'venv/bin/pip')
+WORK_SERVER = 'rabbitmq-server'
+VENV_REQUIREMENTS = 'requirements.txt'
+
+# class Settings(object):
+#     SERVER = "production"
+#     PORT = 22
+#     BRANCH = "dev"
+#     PATH_KEY = [os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')]  # Локальный путь до файла с ключами
+#     PYTHON = os.path.join(PATH, 'venv/bin/python')
+#     PIP = os.path.join(PATH, 'venv/bin/pip')
+#     WORK_SERVER = 'rabbitmq-server'
+#     VENV_REQUIREMENTS = 'requirements.txt'
+
